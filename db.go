058d7ff6bd4b9aa2abd1144c64587f2f7a75af64
omie <intelomkar@gmail.com>
2015-01-11 18:52:41 +0530

    Added basic sqlite handling
    - added methods for insert, update messages in the database
    - added method to get pending messages
    - added sql query to create required tables

---
+++ db.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var db *sql.DB
+
+func initDB(driver, dbname string) error {
+	var err error
+	db, err = sql.Open(driver, dbname)
+	return err
+}
+
+func insertMessage(sms *SMS) error {
+
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	stmt, err := tx.Prepare("INSERT INTO messages(uuid, message, mobile) VALUES(?, ?, ?)")
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(sms.uuid, sms.body, sms.mobile)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	tx.Commit()
+	return nil
+}
+
+func updateMessageStatus(sms SMS) error {
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	stmt, err := tx.Prepare("UPDATE messages SET status=?")
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(sms.status)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	tx.Commit()
+	return nil
+
+}
+
+func getPendingMessages() ([]SMS, error) {
+	rows, err := db.Query("SELECT uuid, message, mobile, status FROM messages WHERE status=" + string(SMSPending))
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	var messages []SMS
+
+	for rows.Next() {
+		sms := SMS{}
+		rows.Scan(&sms.uuid, &sms.body, &sms.mobile, &sms.status)
+		messages = append(messages, sms)
+	}
+	rows.Close()
+	return messages, nil
+}
